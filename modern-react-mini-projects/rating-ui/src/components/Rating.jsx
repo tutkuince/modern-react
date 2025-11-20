a66cc2f263b2a976750f729513b99429d864f70c
tutkuince <tutku.ince@outlook.com>
2025-11-20 22:21:33 +0300

    handling events

--- modern-react-mini-projects/rating-ui/src/components/Rating.jsx
+++ modern-react-mini-projects/rating-ui/src/components/Rating.jsx
@@ -1,12 +1,23 @@
 const Rating = () => {
   const stars = Array.from({ length: 5 }, (_, i) => i + 1);
 
+  const clicked = (index) => console.log("Clicked", index);
+
+  const hovered = (direction, index) =>
+    console.log("Hovered!", direction, index);
+
   return (
     <div className="rating-container">
       <h2>Rater Your Experience</h2>
       <div className="stars">
-        {stars.map((star) => (
-          <span key={star} className="star">
+        {stars.map((star, index) => (
+          <span
+            onClick={() => clicked(index)}
+            onMouseEnter={() => hovered("enter", index)}
+            onMouseLeave={() => hovered("leave", index)}
+            key={star}
+            className="star"
+          >
             {"\u2605"}
           </span>
         ))}
