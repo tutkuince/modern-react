97d81f3e74c6573e404be36154f51f762d9569e9
tutkuince <tutku.ince@outlook.com>
2025-11-20 22:01:37 +0300

    styling in react

--- modern-react-mini-projects/rating-ui/src/App.jsx
+++ modern-react-mini-projects/rating-ui/src/App.jsx
@@ -1,13 +1,8 @@
 import Rating from "./components/Rating";
 
 const App = () => {
-  const name = "Tutku";
-
   return (
     <div>
-      <header>
-        <h1>Hello {name}</h1>
-      </header>
       <Rating />
     </div>
   );
--- modern-react-mini-projects/rating-ui/src/components/Rating.jsx
+++ modern-react-mini-projects/rating-ui/src/components/Rating.jsx
@@ -1,5 +1,9 @@
 const Rating = () => {
-  return <div>Rate Your Experience</div>;
+  return (
+    <div className="rating-container">
+      <h2>Rater Your Experience</h2>
+    </div>
+  );
 };
 
 export default Rating;
