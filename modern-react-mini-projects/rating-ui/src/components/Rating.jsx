e8f78dc4b41925fdea0c14bb86a2555762deb831
tutkuince <tutku.ince@outlook.com>
2025-11-20 22:13:11 +0300

    lists

--- modern-react-mini-projects/rating-ui/src/components/Rating.jsx
+++ modern-react-mini-projects/rating-ui/src/components/Rating.jsx
@@ -1,7 +1,16 @@
 const Rating = () => {
+  const stars = Array.from({ length: 5 }, (_, i) => i + 1);
+
   return (
     <div className="rating-container">
       <h2>Rater Your Experience</h2>
+      <div className="stars">
+        {stars.map((star) => (
+          <span key={star} className="star">
+            {"\u2605"}
+          </span>
+        ))}
+      </div>
     </div>
   );
 };
