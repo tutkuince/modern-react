a974da54abaab884f0ff0450581c7acf3eb7389d
tutkuince <tutku.ince@outlook.com>
2025-11-22 19:58:56 +0300

    Rating state with useState();

--- modern-react-mini-projects/rating-ui/src/components/Rating.jsx
+++ modern-react-mini-projects/rating-ui/src/components/Rating.jsx
@@ -1,23 +1,16 @@
+import { useState } from "react";
+
 const Rating = () => {
+  const [rating, setRating] = useState(0);
+
   const stars = Array.from({ length: 5 }, (_, i) => i + 1);
-
-  const clicked = (index) => console.log("Clicked", index);
-
-  const hovered = (direction, index) =>
-    console.log("Hovered!", direction, index);
 
   return (
     <div className="rating-container">
       <h2>Rater Your Experience</h2>
       <div className="stars">
-        {stars.map((star, index) => (
-          <span
-            onClick={() => clicked(index)}
-            onMouseEnter={() => hovered("enter", index)}
-            onMouseLeave={() => hovered("leave", index)}
-            key={star}
-            className="star"
-          >
+        {stars.map((star) => (
+          <span onClick={() => setRating(star)} key={star} className="star">
             {"\u2605"}
           </span>
         ))}
