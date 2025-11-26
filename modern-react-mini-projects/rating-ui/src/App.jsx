5423b28e7d245f583322cefb8e8b2d53edb3dac7
tutkuince <tutku.ince@outlook.com>
2025-11-26 21:57:11 +0300

    UseState Hook

--- modern-react-mini-projects/rating-ui/src/App.jsx
+++ modern-react-mini-projects/rating-ui/src/App.jsx
@@ -1,9 +1,11 @@
+import Counter from "./components/Counter";
 import Rating from "./components/Rating";
 
 const App = () => {
   return (
     <div>
       <Rating />
+      <Counter />
     </div>
   );
 };
