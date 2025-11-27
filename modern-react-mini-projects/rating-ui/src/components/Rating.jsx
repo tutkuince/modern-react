bbbd05e182db4b51c249663d1b912ae66a8340cd
tutkuince <tutku.ince@outlook.com>
2025-11-27 23:09:51 +0300

    submit rating

--- modern-react-mini-projects/rating-ui/src/components/Rating.jsx
+++ modern-react-mini-projects/rating-ui/src/components/Rating.jsx
@@ -4,9 +4,21 @@ import Star from "./Star";
 const Rating = ({ heading = "Rate Your Experience", color = "gold" }) => {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
+  const [submitted, setSubmitted] = useState(false);
 
   const stars = Array.from({ length: 5 }, (_, i) => i + 1);
   const feedbackMessages = ["Terrible", "Poor", "Fair", "Good", "Excellence"];
+  const handleSubmit = () => {
+    if (rating > 0) {
+      setSubmitted(true);
+    }
+  };
+
+  const closeModel = () => {
+    setSubmitted(false);
+    setRating(0);
+    setHover(0);
+  };
 
   return (
     <div className="rating-container">
@@ -26,6 +38,27 @@ const Rating = ({ heading = "Rate Your Experience", color = "gold" }) => {
         ))}
       </div>
       {rating > 0 && <p className="feedback">{feedbackMessages[rating - 1]}</p>}
+      <button
+        className="submit-btn"
+        onClick={handleSubmit}
+        disabled={rating === 0}
+      >
+        Submit
+      </button>
+      {/* Modal */}{" "}
+      {submitted && (
+        <div className="modal-overlay">
+          <div className="modal">
+            <h2>Thank You</h2>
+            <p>
+              You rated us {rating} star{rating > 1 ? "s" : ""}
+            </p>
+            <button className="close-btn" onClick={closeModel}>
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
