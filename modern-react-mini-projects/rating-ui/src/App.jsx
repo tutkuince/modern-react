c4acfab3fa06cfcab78e1b66000aa18a75533405
tutkuince <tutku.ince@outlook.com>
2025-11-27 21:56:12 +0300

    conditional rendering & styling

--- modern-react-mini-projects/rating-ui/src/App.jsx
+++ modern-react-mini-projects/rating-ui/src/App.jsx
@@ -1,11 +1,9 @@
-import Counter from "./components/Counter";
 import Rating from "./components/Rating";
 
 const App = () => {
   return (
     <div>
       <Rating />
-      <Counter />
     </div>
   );
 };
--- modern-react-mini-projects/rating-ui/src/components/Rating.jsx
+++ modern-react-mini-projects/rating-ui/src/components/Rating.jsx
@@ -2,19 +2,28 @@ import { useState } from "react";
 
 const Rating = () => {
   const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
 
   const stars = Array.from({ length: 5 }, (_, i) => i + 1);
+  const feedbackMessages = ["Terrible", "Poor", "Fair", "Good", "Excellence"];
 
   return (
     <div className="rating-container">
       <h2>Rater Your Experience</h2>
       <div className="stars">
         {stars.map((star) => (
-          <span onClick={() => setRating(star)} key={star} className="star">
+          <span
+            onClick={() => setRating(star)}
+            onMouseEnter={() => setHover(star)}
+            onMouseLeave={() => setHover(0)}
+            key={star}
+            className={`star ${star <= (hover || rating) ? "active" : ""}`}
+          >
             {"\u2605"}
           </span>
         ))}
       </div>
+      {rating > 0 && <p className="feedback">{feedbackMessages[rating - 1]}</p>}
     </div>
   );
 };
