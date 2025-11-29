d179f500ccf3c111c26d0ea92763194e58960d6c
tutkuince <tutku.ince@outlook.com>
2025-11-29 22:37:42 +0300

    Form Submission & Global State

--- modern-react-mini-projects/notes-app/src/App.jsx
+++ modern-react-mini-projects/notes-app/src/App.jsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import NoteForm from "./components/NoteForm";
 
 const App = () => {
+  const [notes, setNotes] = useState([]);
+
   return (
     <div className="max-w-lg mx-auto mt-10 p-6 bg-gray-100 rounded-lg shadow-lg">
       <h2 className="text-2xl font-bold mb-4 text-center">📝Notes App</h2>
-      <NoteForm />
+      <NoteForm notes={notes} setNotes={setNotes} />
     </div>
   );
 };
--- modern-react-mini-projects/notes-app/src/components/NoteForm.jsx
+++ modern-react-mini-projects/notes-app/src/components/NoteForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const NoteForm = () => {
+const NoteForm = ({ notes, setNotes }) => {
   const [formData, setFormData] = useState({
     title: "",
     category: "Work",
@@ -15,8 +15,32 @@ const NoteForm = () => {
     });
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validation
+    if (!formData.title || !formData.description) return;
+
+    // Create note object
+    const newNote = {
+      id: Date.now(),
+      ...formData,
+    };
+
+    // Add notes to state
+    setNotes([newNote, ...notes]);
+
+    // Reset form data
+    setFormData({
+      title: "",
+      category: "Work",
+      priority: "Medium",
+      description: "",
+    });
+  };
+
   return (
-    <form className="mb-6">
+    <form onSubmit={handleSubmit} className="mb-6">
       <div className="mb-4">
         <label htmlFor="title" className="block font-semibold">
           Title
